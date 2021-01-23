7ac4c782c52299d59913f6a9376a11e12b5c96ee
Somya Mohindra <somya.mohindra@berkeley.edu>
2021-01-22 22:36:09 -0800

    fguckign hell

---
+++ Desktop/cs61c/quest/main.c
@@ -0,0 +1,8 @@
+#include <inttypes.h>
+
+extern uint64_t bit_manip(uint64_t);
+
+int main(int argc, char *argv[]) {
+    bit_manip(0b1111111111111111111111111111111111111111111111111111111111111111)
+    return 0;
+}
